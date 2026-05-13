e5992446daa41606bfbe5be5c647c7008614bfed
Joseph McSweeney <163135970+Joseph-MCS@users.noreply.github.com>
2026-05-13 02:08:29 +0200

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,15 @@
+
+integer_type = 6
+float_type = 143.4343
+bool_type = False
+string_type = "I hate SuperValu"
+list_type = [1,1,11,1,1,1,1,1,11,1,1,1,11,1,1,1,11,1,1,1,1,11,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1,1,32,32,32,32,3,23,23,2,3,23,23,23,2,3,2,32,3,2,32,3,2,32,3,34,5,45,564,5,435,43,5]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
